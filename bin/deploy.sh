9a124234138a2cf83a06e46e7b7fc3505f182172
Jason Chown <j.chown@vtime.net>
2023-09-24 17:26:12 +0100

    Ah, do need the install

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,6 +5,7 @@ set -e
 docker="docker run --rm -v $(pwd)/:/app/ -w /app"
 yarn="$docker node:18-alpine yarn"
 
+$yarn install
 $yarn build
 
 aws="$docker -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
