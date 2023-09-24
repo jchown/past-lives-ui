f8c88a3e6bbf37964e6c9c4b77e12f6309310fc9
Jason Chown <j.chown@vtime.net>
2023-09-24 20:01:58 +0100

    Think invalidation fixed

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker="docker run --rm -v $(pwd)/:/app/ -w /app"
+docker="docker run --rm -i -v $(pwd)/:/app/ -w /app"
 yarn="$docker node:18-alpine yarn"
 aws="$docker -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
 jq="$docker stedolan/jq"
@@ -19,7 +19,7 @@ $aws s3 --region eu-west-2 sync dist/ s3://www.datasmelter.com/history/past-live
 echo "Invalidating CDN"
 
 $aws cloudfront create-invalidation --region eu-west-2 --distribution-id E3LWUSSIKV8P0V --paths /history/\* --region eu-west-2 >invalidation.json
-invalidation_id=$($jq -f invalidation.json -r '.Invalidation.Id')
+invalidation_id=$(cat invalidation.json | $jq -r '.Invalidation.Id')
 
 echo "Waiting for invalidation $invalidation_id"
 
