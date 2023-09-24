28dc87680338075e45b96abba2ee7298d3c71fd4
Jason Chown <j.chown@vtime.net>
2023-09-24 16:30:55 +0100

    Add invalidation (after finding old build script)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,9 +5,12 @@ set -e
 docker="docker run --rm -v $(pwd)/:/app/ -w /app"
 yarn="$docker node:18-alpine yarn"
 
-$yarn install
 $yarn build
 
 aws="$docker -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
 
 $aws s3 --region eu-west-2 sync dist/ s3://www.datasmelter.com/history/past-lives/ --delete --acl public-read
+
+invalidation_output=$($docker cloudfront create-invalidation --distribution-id E3LWUSSIKV8P0V --paths /history/\*)
+invalidation_id=$(echo $invalidation_output | jq -r '.Invalidation.Id')
+$docker cloudfront wait invalidation-completed --id $invalidation_id --distribution-id E3LWUSSIKV8P0V
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-docker run --rm -v ${PWD}:/app -w /app -e GENERATE_SOURCEMAP=false -e NODE_OPTIONS="--max-old-space-size=8192" node:18 yarn build
-docker run --rm -v ${PWD}/dist:/dist -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli s3 sync /dist s3://www.datasmelter.com/history/past-lives --acl public-read --delete
-invalidation_output=$(docker run --rm -v ${PWD}/dist:/dist -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli cloudfront create-invalidation --distribution-id E3LWUSSIKV8P0V --paths /history/\*)
-invalidation_id=$(echo $invalidation_output | jq -r '.Invalidation.Id')
-echo "Waiting for invalidation $invalidation_id to complete"
-docker run --rm -v ${PWD}/dist:/dist -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli cloudfront wait invalidation-completed --id $invalidation_id --distribution-id E3LWUSSIKV8P0V
