1b37b08ca871097a9e54d0345515caa83a17f5e5
Jason Chown <j.chown@vtime.net>
2023-09-24 18:31:57 +0100

    Fix jq

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,7 @@ set -e
 docker="docker run --rm -i -v $(pwd)/:/app/ -w /app"
 yarn="$docker node:18-alpine yarn"
 aws="$docker -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
-jq="$docker ghcr.io/docwhat/jq-docker"
+jq="$docker stedolan/jq"
 
 echo "Building site"
 
@@ -19,7 +19,7 @@ $aws s3 --region eu-west-2 sync dist/ s3://www.datasmelter.com/history/past-live
 echo "Invalidating CDN"
 
 invalidation_output=$($aws cloudfront create-invalidation --distribution-id E3LWUSSIKV8P0V --paths /history/\*)
-invalidation_id=$(echo $invalidation_output | $jq '.Invalidation.Id')
+invalidation_id=$(echo $invalidation_output | $jq -r '.Invalidation.Id')
 
 echo "Waiting for invalidation $invalidation_id"
 
