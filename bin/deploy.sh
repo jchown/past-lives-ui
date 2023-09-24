a972ed463f9e9b95dfb5bea18a84e996feea1f02
Jason Chown <j.chown@vtime.net>
2023-09-24 15:58:58 +0100

    Fix the right file

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-yarn install
-yarn build
+$yarn="docker run --rm -t -v $(pwd)/:/usr/app/ -w /usr/app node:18-alpine yarn"
+
+$yarn install
+$yarn build
 
 aws="docker run --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
 
-$aws s3 --region eu-west-2 sync /usr/app/.output/public/ s3://games-webpage --delete --acl public-read
+$aws s3 --region eu-west-2 sync dist/ s3://www.datasmelter.com/history/past-lives/ --delete --acl public-read
