79602e89b08cd2a219773eba7f770fb50b96a73f
Jason Chown <j.chown@vtime.net>
2023-09-24 16:19:23 +0100

    Uses the same paths to avoid confusions

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-yarn="docker run --rm -t -v $(pwd)/:/usr/app/ -w /usr/app node:18-alpine yarn"
+docker="docker run --rm -v $(pwd)/:/app/ -w /app"
+yarn="$docker node:18-alpine yarn"
 
 $yarn install
 $yarn build
 
-aws="docker run --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
+aws="$docker -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
 
-$aws s3 --region eu-west-2 sync /usr/app/dist/ s3://www.datasmelter.com/history/past-lives/ --delete --acl public-read
+$aws s3 --region eu-west-2 sync dist/ s3://www.datasmelter.com/history/past-lives/ --delete --acl public-read
