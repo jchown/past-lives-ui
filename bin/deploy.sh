c75f9834e6e590694dedb4f7ddf330719d9abcc8
Jason Chown <j.chown@vtime.net>
2023-09-24 15:39:32 +0100

    Use a script to build & deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+yarn install
+yarn build
+
+aws="docker run --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
+
+$aws s3 --region eu-west-2 sync /usr/app/.output/public/ s3://games-webpage --delete --acl public-read
