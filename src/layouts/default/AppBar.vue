efe071e3ed05240c22175de5e9569e7bd433fb09
Jason Chown <j.chown@vtime.net>
2023-09-26 21:53:34 +0100

    Use abs paths and add a logo

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app-bar :elevation="2">
     <v-app-bar-title>
-      <v-icon icon="mdi-calendar" />
 
+      <img src="@/assets/triskelion.png" alt="Triskelion" height="20" width="20" style="margin-right: 12px;"/>
       <span>
-        <router-link to="/">Past Lives</router-link>
+        <router-link to="/" style="text-decoration: none; color: inherit; font-size: 20pt;">Past Lives</router-link>
       </span>
 
     </v-app-bar-title>
@@ -12,5 +12,4 @@
 </template>
 
 <script lang="ts" setup>
-
 </script>
